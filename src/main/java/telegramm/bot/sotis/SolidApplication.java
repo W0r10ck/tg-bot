cb183e6367843e1e59dc501f2c9002f7f5c7639e
moa <adic-94@mail.com>
2024-09-14 21:21:59 +0300

    change name

--- src/main/java/telegramm/bot/sotis/SolidApplication.java
+++ src/main/java/telegramm/bot/sotis/SolidApplication.java
@@ -1,4 +1,4 @@
-package telegramm.bot.solid;
+package telegramm.bot.sotis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
